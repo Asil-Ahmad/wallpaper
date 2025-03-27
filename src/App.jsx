d6b692ccf35de53110c3d4971afdec082f9bda22
Asil Ahmad <identity101@duck.com>
2025-03-27 06:14:07 +0530

    refactor App component to remove lazy loading for Homepage and Alvida, and simplify loading logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
+import React, { useState, useEffect } from "react";
+import Homepage from "./components/Homepage";
 import Loader from "./constant/Loader";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-
-const Homepage = lazy(() => import("./components/Homepage"));
-const Alvida = lazy(() => import("./components/Alvida"));
+import Alvida from "./components/Alvida";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -12,7 +11,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); // Add a delay (e.g., 2000ms)
+    }, );
 
     return () => clearTimeout(timer);
   }, []);
@@ -24,12 +23,10 @@ const App = () => {
       ) : (
         <>
           <Navbar />
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path='/' element={<Homepage />} />
-              <Route path='/alvida' element={<Alvida />} />
-            </Routes>
-          </Suspense>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/alvida' element={<Alvida/>} />
+          </Routes>
         </>
       )}
     </div>
